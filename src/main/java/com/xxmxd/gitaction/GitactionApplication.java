3d4d9589d1bb79f556d7933f7998eb9c034ff315
qjp1993 <237386254@qq.com>
2023-09-22 00:12:45 +0800

    init

---
+++ src/main/java/com/xxmxd/gitaction/GitactionApplication.java
@@ -0,0 +1,17 @@
+package com.xxmxd.gitaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitactionApplication {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("OK!");
+
+        SpringApplication.run(GitactionApplication.class, args);
+    }
+
+}
